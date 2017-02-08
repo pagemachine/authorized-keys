edf76d7fde2cedde5a8d6bdeaf21735986a59dfb
Mathias Brodala <mbrodala@pagemachine.de>
2017-02-08 12:05:55 +0100

    [TASK] Test public key changes

--- test/unit/PublicKeyTest.php
+++ test/unit/PublicKeyTest.php
@@ -67,4 +67,30 @@ FILE;
     $this->assertEquals('AAAAB3NzaC1yc2EAAAADAQABAAABAQDFy1wC52dQBLnJ8dwQCsTwTuDwCQAhb/2joe6oK4Qm6XBI89BerXTsTvV8ekxg3LjvD6LjclJR6WsDQPA8cJeKXl/XDtcd+a355fth1sRZwe20Zh7NrpfhGD8Pb4HWrnJz0jeVXn5M/FppvRFl4RX7dhz5zuHFIb8BeCOmoNid1vTucp9HCr9PkCcahRpw4QXU5v2ETXbbxmftGz7PBvGHR2In1nm3MBBlX++11sDhlYUCWqJXjfH0dvgpWvEtknJoyHjX8MvNV6oXeh59ow6unIOJjXPkdyICXjZCJtBdVK2pc3mYKxaDWNN7MvLelduw941CXaa4aE2EFDa0BLTJ', $publicKey->getKey());
     $this->assertEquals('mbrodala@pagemachine.de', $publicKey->getComment());
   }
+
+  /**
+   * @test
+   */
+  public function setsKeyParts() {
+
+    $key = <<<FILE
+command="/bin/test" ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDFy1wC52dQBLnJ8dwQCsTwTuDwCQAhb/2joe6oK4Qm6XBI89BerXTsTvV8ekxg3LjvD6LjclJR6WsDQPA8cJeKXl/XDtcd+a355fth1sRZwe20Zh7NrpfhGD8Pb4HWrnJz0jeVXn5M/FppvRFl4RX7dhz5zuHFIb8BeCOmoNid1vTucp9HCr9PkCcahRpw4QXU5v2ETXbbxmftGz7PBvGHR2In1nm3MBBlX++11sDhlYUCWqJXjfH0dvgpWvEtknJoyHjX8MvNV6oXeh59ow6unIOJjXPkdyICXjZCJtBdVK2pc3mYKxaDWNN7MvLelduw941CXaa4aE2EFDa0BLTJ mbrodala@pagemachine.de
+FILE;
+
+    $publicKey = new PublicKey($key);
+
+    $publicKey->setOptions('agent-forwarding');
+    $this->assertEquals('agent-forwarding', $publicKey->getOptions());
+
+    $publicKey->setType('ssh-dss');
+    $this->assertEquals('ssh-dss', $publicKey->getType());
+
+    $publicKey->setKey('QWERTZ');
+    $this->assertEquals('QWERTZ', $publicKey->getKey());
+
+    $publicKey->setComment('Test');
+    $this->assertEquals('Test', $publicKey->getComment());
+
+    $this->assertEquals('agent-forwarding ssh-dss QWERTZ Test', (string) $publicKey);
+  }
 }
