d93af43a0e279f85c4c75b88fff61911055d92fa
Mathias Brodala <mbrodala@pagemachine.de>
2017-02-08 11:05:11 +0100

    [FEATURE] Parse public key parts

--- src/PublicKey.php
+++ src/PublicKey.php
@@ -18,18 +18,86 @@ namespace pagemachine\AuthorizedKeys;
 class PublicKey {
 
   /**
-   * Key string
-   *
-   * @var string
+   * @var string $type
+   */
+  protected $type;
+
+  /**
+   * @var string $key
    */
   protected $key;
+
+  /**
+   * @var string $comment
+   */
+  protected $comment;
 
   /**
    * @param string $key public key string
    */
   public function __construct($key) {
 
+    $parts = explode(' ', $key);
+    array_map('trim', $parts);
+
+    $this->type = $parts[0];
+    $this->key = $parts[1];
+
+    if (isset($parts[2])) {
+
+      $this->comment = $parts[2];
+    }
+  }
+
+  /**
+   * @return string
+   */
+  public function getType() {
+
+    return $this->type;
+  }
+
+  /**
+   * @param string $type
+   * @return void
+   */
+  public function setType($type) {
+
+    $this->type = $type;
+  }
+
+  /**
+   * @return string
+   */
+  public function getKey() {
+
+    return $this->key;
+  }
+
+  /**
+   * @param string $key
+   * @return void
+   */
+  public function setKey($key) {
+
     $this->key = $key;
+  }
+
+  /**
+   * @return string
+   */
+  public function getComment() {
+
+    return $this->comment;
+  }
+
+  /**
+   * @param string $comment
+   * @return void
+   */
+  public function setComment($comment) {
+
+    $this->comment = $comment;
   }
 
   /**
@@ -39,6 +107,13 @@ class PublicKey {
    */
   public function __toString() {
 
-    return $this->key;
+    $parts = [$this->type, $this->key];
+
+    if (!empty($this->comment)) {
+
+      $parts[] = $this->comment;
+    }
+
+    return implode(' ', $parts);
   }
 }
--- test/unit/PublicKeyTest.php
+++ test/unit/PublicKeyTest.php
@@ -33,4 +33,20 @@ FILE;
 
     $this->assertEquals($key, (string) $publicKey);
   }
+
+  /**
+   * @test
+   */
+  public function parsesKeyParts() {
+
+    $key = <<<FILE
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDFy1wC52dQBLnJ8dwQCsTwTuDwCQAhb/2joe6oK4Qm6XBI89BerXTsTvV8ekxg3LjvD6LjclJR6WsDQPA8cJeKXl/XDtcd+a355fth1sRZwe20Zh7NrpfhGD8Pb4HWrnJz0jeVXn5M/FppvRFl4RX7dhz5zuHFIb8BeCOmoNid1vTucp9HCr9PkCcahRpw4QXU5v2ETXbbxmftGz7PBvGHR2In1nm3MBBlX++11sDhlYUCWqJXjfH0dvgpWvEtknJoyHjX8MvNV6oXeh59ow6unIOJjXPkdyICXjZCJtBdVK2pc3mYKxaDWNN7MvLelduw941CXaa4aE2EFDa0BLTJ mbrodala@pagemachine.de
+FILE;
+
+    $publicKey = new PublicKey($key);
+
+    $this->assertEquals('ssh-rsa', $publicKey->getType());
+    $this->assertEquals('AAAAB3NzaC1yc2EAAAADAQABAAABAQDFy1wC52dQBLnJ8dwQCsTwTuDwCQAhb/2joe6oK4Qm6XBI89BerXTsTvV8ekxg3LjvD6LjclJR6WsDQPA8cJeKXl/XDtcd+a355fth1sRZwe20Zh7NrpfhGD8Pb4HWrnJz0jeVXn5M/FppvRFl4RX7dhz5zuHFIb8BeCOmoNid1vTucp9HCr9PkCcahRpw4QXU5v2ETXbbxmftGz7PBvGHR2In1nm3MBBlX++11sDhlYUCWqJXjfH0dvgpWvEtknJoyHjX8MvNV6oXeh59ow6unIOJjXPkdyICXjZCJtBdVK2pc3mYKxaDWNN7MvLelduw941CXaa4aE2EFDa0BLTJ', $publicKey->getKey());
+    $this->assertEquals('mbrodala@pagemachine.de', $publicKey->getComment());
+  }
 }
