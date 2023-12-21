aa7b33385c99ce806a9ab4ac04b755cedaa097ee
Mathias Brodala <mbrodala@pagemachine.de>
2023-12-21 15:25:04 +0100

    [TASK] Set up PHP-CS-Fixer config

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,19 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->ignoreVCSIgnored(true)
+    ->exclude([
+        'vendor',
+    ])
+;
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR12' => true,
+        '@PSR12:risky' => true,
+        '@PHP74Migration' => true,
+    ])
+    ->setFinder($finder)
+;
